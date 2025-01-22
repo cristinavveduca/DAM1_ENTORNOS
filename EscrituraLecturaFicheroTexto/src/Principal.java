cb74b98c310c6e59075cf3a3b51d2cdaafe9daf5
davidbr03 <david.bolanos@educa.madrid.org>
2025-01-22 13:24:10 +0100

    Escritura y Lectura de fichero actualizado

--- EscrituraLecturaFicheroTexto/src/Principal.java
+++ EscrituraLecturaFicheroTexto/src/Principal.java
@@ -6,24 +6,26 @@ import java.io.PrintWriter;
 public class Principal {
     public static void main(String[] args) {
 
+        //Escritura en el fichero datos.txt
         System.out.println("\nESCRITURA EN DATOS.TXT: ");
         PrintWriter ficheroEscritura = null;
         try {
             ficheroEscritura = new PrintWriter(new FileWriter("datos.txt"));
             for (int i = 1; i <= 10; i++) {
                 System.out.println("Escribiendo número: " + i);
-                ficheroEscritura.println(i);
+                ficheroEscritura.println(i); //Escribimos los numeros del 1 al 10 por linea en cada iteraccion el fichero datos.txt
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                if (ficheroEscritura != null) ficheroEscritura.close();
+                if (ficheroEscritura != null) ficheroEscritura.close(); //Cerramos el fichero
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
+        //Lectura en el fichero datos.txt
         System.out.println("\nLECTURA DE DATOS.TXT: ");
         BufferedReader ficheroLectura = null;
 
@@ -33,13 +35,13 @@ public class Principal {
 
             while (linea != null) {
                 System.out.println("Leído número: " + linea);
-                linea = ficheroLectura.readLine();
+                linea = ficheroLectura.readLine(); //Leemos las lineas que hemos escrito anteriormente
             }
         } catch (Exception e) {
             System.out.println("Error: " + e);
         } finally {
             try {
-                if (ficheroLectura != null) ficheroLectura.close();
+                if (ficheroLectura != null) ficheroLectura.close(); //Cerramos el fichero al finalizar
             } catch (Exception e) {
                 System.out.println("Error: " + e);
             }
